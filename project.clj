e6fa8aef1078945d271dacd2ef1083aa2b9eeca9
Stig Brautaset <stig@brautaset.org>
2022-06-19 19:50:27 +0100

    Add orchestra dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [orchestra "2021.01.01-1"]]
   :repl-options {:init-ns ports-and-adapters.core})
